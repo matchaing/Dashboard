c75a83bb0b7a5b672a1270fb12adeced5fb6bf90
matchaing <20191743@edu.hanbat.ac.kr>
2022-09-25 23:28:52 +0900

    cached

--- src/components/DisplayData.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function DisplayData() {
-    fetch('https://api.github.com/orgs/nodejs')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
-export default DisplayData;
