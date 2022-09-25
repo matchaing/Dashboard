a040431c976a32f9684d5b668bb5f5a0ae2ee841
matchaing <20191743@edu.hanbat.ac.kr>
2022-09-25 23:30:25 +0900

    commit

---
+++ src/pages/Detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+
+class Detail extends React.Component {
+    render () {
+        return (
+            <div className="page">데이터 상세 분석</div>
+        )
+    }
+}
+
+export default Detail;
---
+++ src/pages/Risk.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+
+class Risk extends React.Component {
+    render () {
+        return (
+            <div className="page">위험도 분석</div>
+        )
+    }
+}
+
+export default Risk;
